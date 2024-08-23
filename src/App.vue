46a6d13e20e26b914be8bb20f8e8bc2aa0a2c802
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-08-23 12:53:16 +0200

    chore: add checkbox + 2 initial tasks

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,29 @@
   <form actions="" @submit.prevent="addTodo">
     <fieldset role="group">
       <input
-        v-model="todo"
+        v-model="newTodo"
         type="text"
         placeholder="Tasks to do:">
-      <button>Add</button>
+      <button :disabled="newTodo.length === 0">Add</button>
   </fieldset>
   </form>
 
   <div v-if="todos.length === 0">No task to do</div>
+  <div v-else>
+    <ul>
+      <li
+      v-for="todo in todos"
+      :key="todo.date"
+      >
+      <label>
+        <input type="checkbox" v-model="todo.completed">
+        <!-- If checked, done becomes true and vice versa  -->
+        {{ todo.title }}
+      </label>
+    </li>
+
+    </ul>
+  </div>
 </template>
 
 
@@ -18,16 +33,27 @@
 
 import { ref } from 'vue'
 
-const todo = ref('') // Task to do
-const todos = ref([]) // Array of tasks
+const newTodo = ref('') // Task to do
+const todos = ref([{
+  title: 'Finish Fanprime technical test',
+  done: true,
+  date: Date.now()
+}, {
+  title: 'Go to the gym',
+  done: false,
+  date: Date.now()
+}]) // Array of tasks
+
 const addTodo = () => {
   // Method to add a new task
   todos.value.push({
-    title: todo.value,
+    title: newTodo.value,
     done: false,
     date: Date.now()
   })
 }
+
+newTodo.value = '' // Reset the input field
 
 </script>
 
